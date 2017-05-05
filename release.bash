024b78290f0b8fd13a3bf57790bdded977656493
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-05 23:27:05 +0000

    bolster release script

--- release.bash
+++ release.bash
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 fi
 
 # move to working directory
-cd $( dirname "${BASH_SOURCE[0]}" )/../
+cd $( dirname "${BASH_SOURCE[0]}" )
 
 # add version to script
 sed -i "s/namespace gpgl\\\console;/namespace gpgl\\\console;\n\$version = '$1';/" bin/gpgl
@@ -16,7 +16,9 @@ sed -i "s/namespace gpgl\\\console;/namespace gpgl\\\console;\n\$version = '$1';
 composer install --no-dev
 
 # https://github.com/clue/phar-composer
-phar-composer build .
+phar-composer build . gpgl.phar
+
+gpg --detach-sign --output gpgl.phar.asc gpgl.phar
 
 # undo hard-coded version in script
 git reset --hard
