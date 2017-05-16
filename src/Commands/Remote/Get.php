5b4afaf6849204776e982f76b15c7b4730bceb39
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-16 18:43:15 +0000

    add remote get command

--- src/Commands/Remote/Get.php
+++ src/Commands/Remote/Get.php
@@ -1,66 +1,37 @@
 <?php
 
-namespace gpgl\console\Commands;
+namespace gpgl\console\Commands\Remote;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use gpgl\console\Commands\Traits\DatabaseGateway;
 
-class Remote extends Command {
+class Get extends Command {
     use DatabaseGateway;
 
     protected function configure()
     {
         $this
             // the name of the command (the part after "bin/console")
-            ->setName('push')
+            ->setName('remote:get')
 
             // the short description shown while running "php bin/console list"
-            ->setDescription('Push database to server')
+            ->setDescription('Get remote credentials')
 
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('Pushes the database to a remote server.')
+            ->setHelp('Lists remote aliases or URL and token for a given remote.')
 
             ->addDatabaseOption()
 
-            ->addOption(
-                'alias',
-                'a',
-                InputOption::VALUE_REQUIRED,
-                'Alias name for remote'
-            )
-
-            ->addOption(
-                'remote',
-                'r',
-                InputOption::VALUE_REQUIRED,
-                'URL to remote server'
-            )
-
-            ->addOption(
-                'token',
-                't',
-                InputOption::VALUE_REQUIRED,
-                'Authorization token to remote server'
-            )
-
-            ->addOption(
-                'save',
-                's',
-                InputOption::VALUE_NONE,
-                'Save the remote. Requires --name, --remote, and --token options'
-            )
-
-            ->addOption(
-                'save-default',
-                'D',
-                InputOption::VALUE_NONE,
-                'Save the remote as default. Requires --name, --remote, and --token options'
-            )
+            ->addArgument(
+            'alias',
+            InputArgument::OPTIONAL,
+            'Specific remote for which to return credentials'
+        )
         ;
     }
 
@@ -68,29 +39,14 @@ class Remote extends Command {
     {
         $dbms = $this->accessDatabase($input, $output);
 
-        $token = $input->getOption('token');
+        $alias = $input->getArgument('alias');
 
-        $remote = $input->getOption('remote');
-
-        $gpgldb = fopen($dbms->getFilename(), 'r');
-
-        $headers = [
-            'Authorization' => "Bearer $token",
-        ];
-
-        $client = new \GuzzleHttp\Client;
-
-        $response = $client->put($remote, [
-            'headers' => $headers,
-            'body' => $gpgldb,
-        ]);
-
-        $io = new SymfonyStyle($input, $output);
-
-        if ($response->getStatusCode() === 204) {
-            return $io->success('push successful');
+        if (empty($alias)) {
+            $remote = $dbms->remote();
+        } else {
+            $remote = $dbms->remote()->get($alias);
         }
 
-        return $io->error('push failed');
+        return $output->writeln(json_encode($remote, JSON_PRETTY_PRINT));
     }
 }
