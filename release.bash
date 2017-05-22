dc6918383c284bdab81d5dfafdd9197c91bf5f1a
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-22 21:26:14 +0000

    embed release version number in source code

--- release.bash
+++ release.bash
@@ -11,18 +11,24 @@ fi
 cd $( dirname "${BASH_SOURCE[0]}" )
 
 # add version to script
-sed -i "s/namespace gpgl\\\console;/namespace gpgl\\\console;\n\$version = '$1';/" bin/gpgl
+sed -r -i "s/\\\$version = '.+\+dev'/\$version = '$1'/" bin/gpgl
 
+# tag release code
+git commit -am "release version $1"
+git tag -s "$1"
+
+# minimize distribution
 composer install --no-dev
 
 # https://github.com/clue/phar-composer
 phar-composer build . gpgl.phar
 
+# sign distributable binary
 gpg --detach-sign --output gpgl.phar.asc gpgl.phar
 
-# undo hard-coded version in script
-git reset --hard
-
-git tag -s "$1"
-
+# re-install dev dependencies
 composer install
+
+# bump version
+sed -i "s/\$version = '$1'/\$version = '$1+dev'/" bin/gpgl
+git commit -am "bump version $1+dev"
